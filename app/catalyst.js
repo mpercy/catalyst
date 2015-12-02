9cbe2e199a34df8ab15cf5c6ce42aa0fb14bf3f0
Mike Percy <mpercy@cloudera.com>
2015-12-02 02:46:23 -0800

    Persist TODO list in localStorage

--- app/catalyst.js
+++ app/catalyst.js
@@ -1,18 +1,19 @@
-// TODO: This should live in localStorage.
-var TODO_LIST = [
-  { text: 'Item 1', created: 0, finished: 0, isDone: true },
-  { text: 'Item 2', created: 0, finished: 0, isDone: false },
-  { text: 'Item 3', created: 0, finished: 0, isDone: false }
-];
 
-var currentTimeStr = "";
+// Constants.
+var TEXT = 't';
+var TIME_CREATED = 'c';
+var TIME_FINISHED = 'f';
+var IS_DONE = 'd';
+var ACTIVE_TODOS = 'active_todos';
+
+var g_current_time_str = "";
 
 function updateTime() {
   var d = new Date();
   var time = d.getHours() + ":" + d.getMinutes();
-  if (time != currentTimeStr) {
-    currentTimeStr = time;
-    $("#time").text(currentTimeStr);
+  if (time != g_current_time_str) {
+    g_current_time_str = time;
+    $("#time").text(g_current_time_str);
   }
   setTimeout(updateTime, 1000);
 }
@@ -26,25 +27,45 @@ function toggleTodoList(e) {
   }
 }
 
-function renderTodoList() {
+function renderTodoList(todoList) {
   var content = '';
-  for (var i = 0; i < TODO_LIST.length; i++) {
-    var item = TODO_LIST[i];
+  for (var i = 0; i < todoList.length; i++) {
+    var todo = todoList[i];
     var id = "todo_item_" + i;
-    content += '<li class="' + (item.isDone ? 'done' : '') + '">' +
+    content += '<li class="' + (todo[IS_DONE] ? 'done' : '') + '">' +
                '<img src="delete96.svg">' +
                '<input type="checkbox" id="' + id + '"' +
-               (item.isDone ? ' checked' : '') + '>' +
-               '<label for="' + id + '">' + item.text + '</label>' +
+               (todo[IS_DONE] ? ' checked' : '') + '>' +
+               '<label for="' + id + '">' + todo[TEXT] + '</label>' +
                '</li>' + "\n";
   }
-  $("#todo_list h3").text(TODO_LIST.length + ' to do');
+  $("#todo_list h3").text(todoList.length + ' to do');
   $("#todo_list ul").html(content);
 }
 
+function initTodoList() {
+  var activeTodos = [];
+  var activeTodosEnc = localStorage.getItem(ACTIVE_TODOS);
+  if (activeTodosEnc !== null) {
+    activeTodos = JSON.parse(activeTodosEnc);
+  } else {
+    // Init empty list.
+    localStorage.setItem(ACTIVE_TODOS, JSON.stringify(activeTodos));
+  }
+  renderTodoList(activeTodos);
+}
+
 function appendTodoToList(text) {
-  TODO_LIST.push({text: text, created: new Date().getTime(), isDone: false });
-  renderTodoList();
+  var todo = {};
+  todo[TEXT] = text;
+  todo[TIME_CREATED] = new Date().getTime();
+  todo[TIME_FINISHED] = 0;
+  todo[IS_DONE] = false;
+
+  var todoList = JSON.parse(localStorage.getItem(ACTIVE_TODOS));
+  todoList.push(todo);
+  localStorage.setItem(ACTIVE_TODOS, JSON.stringify(todoList));
+  renderTodoList(todoList);
 }
 
 function handleEnterInTodoBox(e) {
@@ -61,7 +82,7 @@ function handleEnterInTodoBox(e) {
 
 $(document).ready(function() {
   updateTime();
-  renderTodoList();
+  initTodoList();
   $("#todo_toggle").click(toggleTodoList);
   $("#new_todo").keydown(handleEnterInTodoBox);
 });
