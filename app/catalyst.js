74a1d09a0caadb021977851300b7a5862b668970
Mike Percy <mpercy@cloudera.com>
2015-12-02 03:13:24 -0800

    Add support for marking todo items completed and deleting them

--- app/catalyst.js
+++ app/catalyst.js
@@ -10,13 +10,15 @@ var g_current_time_str = "";
 
 function updateTime() {
   var d = new Date();
-  var time = d.getHours() + ":" + d.getMinutes();
+  var minutes = ("0" + d.getMinutes()).slice(-2); // No sprintf() in JS.
+  var time = d.getHours() + ":" + minutes;
   if (time != g_current_time_str) {
     g_current_time_str = time;
     $("#time").text(g_current_time_str);
   }
   setTimeout(updateTime, 1000);
 }
+
 function toggleTodoList(e) {
   e.preventDefault();
   var list = $("#todo_list");
@@ -25,6 +27,43 @@ function toggleTodoList(e) {
   } else {
     list.css("display", "none");
   }
+}
+
+var OP_FINISHED = 1;
+var OP_REOPENED = 2;
+var OP_DELETED = 3;
+
+function updateTodoListItem(index, op) {
+  var todoList = JSON.parse(localStorage.getItem(ACTIVE_TODOS));
+  switch (op) {
+    case OP_FINISHED:
+      todoList[index][IS_DONE] = true;
+      todoList[index][TIME_FINISHED] = new Date().getTime();
+      break;
+    case OP_REOPENED:
+      todoList[index][IS_DONE] = false;
+      break;
+    case OP_DELETED:
+      todoList.splice(index, 1);
+      break;
+    default:
+      break;
+  }
+  localStorage.setItem(ACTIVE_TODOS, JSON.stringify(todoList));
+  renderTodoList(todoList);
+}
+
+function handleTodoToggleFinished(e) {
+  var id = this.id;
+  var index = parseInt(id.substr(10));
+  var op = this.checked ? OP_FINISHED : OP_REOPENED;
+  updateTodoListItem(index, op);
+}
+
+function handleTodoDeleted(e) {
+  var id = $(this).siblings("input").attr("id");
+  var index = parseInt(id.substr(10));
+  updateTodoListItem(index, OP_DELETED);
 }
 
 function renderTodoList(todoList) {
@@ -41,6 +80,8 @@ function renderTodoList(todoList) {
   }
   $("#todo_list h3").text(todoList.length + ' to do');
   $("#todo_list ul").html(content);
+  $("#todo_list ul li input[type='checkbox']").click(handleTodoToggleFinished);
+  $("#todo_list ul li img").click(handleTodoDeleted);
 }
 
 function initTodoList() {
